5ec54977fc2d1287c802af6dbd8e945cca6b4e5c
Karl Cridland <karlcridland@gmail.com>
2021-05-15 00:05:39 +0100

    Comments added.

--- Pokedex/UIImageView.swift
+++ Pokedex/UIImageView.swift
@@ -11,7 +11,7 @@ import UIKit
 extension UIImageView{
     
     // Downloads the contents of a URL to a UIImageView's image, the UIImage that's retrieved is then called back to the
-    // original code. This is used in cases of storing the image to the Pokedex's array of pokemon's images.
+    // original code. This is used for cases of storing the image to the Pokedex's array of pokemon's images.
     
     func downloadImage(url: URL, callback: @escaping (_ image: UIImage)->Void) {
         DispatchQueue.global().async { [weak self] in
